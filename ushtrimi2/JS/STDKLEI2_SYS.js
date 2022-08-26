1704baaf99a5e48a1f367d693cf06b79ed46c480
FACILIZATION\kleardo.dura <kleardodura13@gmail.com>
2022-08-26 16:24:40 +0200

    no message

---
+++ ushtrimi2/JS/STDKLEI2_SYS.js
@@ -0,0 +1,168 @@
+/***************************************************************************************************************************
+**  This source is part of the FLEXCUBE Software Product. 
+**  Copyright (c) 2008 ,2022, Oracle and/or its affiliates.
+**  All rights reserved.
+**  
+**  No part of this work may be reproduced, stored in a retrieval system, 
+**  adopted or transmitted in any form or by any means, electronic, mechanical, photographic, 
+**  graphic, optic recording or otherwise, translated in any language or computer language, 
+**  without the prior written permission of Oracle and/or its affiliates.
+**  
+**  Oracle Financial Services Software Limited.
+**  Oracle Park, Off Western Express Highway,
+**  Goregaon (East),
+**  Mumbai - 400 063,
+**  India.
+**  
+**  Written by         : 
+**  Date of creation   : 
+**  File Name          : STDKLEI2_SYS.js
+**  Purpose            : 
+**  Called From        : 
+**  
+**  CHANGE LOG
+**  Last Modified By   : 
+**  Last modified on   : 
+**  Full Version       : 
+**  Reason             : 
+****************************************************************************************************************************/
+
+//***** Code for criteria Search *****
+var criteriaSearch  = '';
+//----------------------------------------------------------------------------------------------------------------------
+//***** FCJ XML FOR THE SCREEN *****
+//----------------------------------------------------------------------------------------------------------------------
+var fieldNameArray = {"BLK_KLEI2":"CATEGORY~DESCRIPTION~PRODUCT_RESTRICTION~RELATIONSHIP~MAKER~MAKERSTAMP~CHECKER~CHECKERSTAMP~MODNO~TXNSTAT~AUTHSTAT~ONCEAUTH","BLK_KLEI2_DETAILS":"PRODUCT_CODE~RELATIONSHIP"};
+
+var multipleEntryPageSize = {"BLK_KLEI2_DETAILS" :"15" };
+
+var multipleEntrySVBlocks = "";
+
+var tabMEBlks = {"CVS_KLEI2__TAB_MAIN":"BLK_KLEI2_DETAILS"};
+
+var msgxml=""; 
+msgxml += '    <FLD>'; 
+msgxml += '      <FN PARENT="" RELATION_TYPE="1" TYPE="BLK_KLEI2">CATEGORY~DESCRIPTION~PRODUCT_RESTRICTION~RELATIONSHIP~MAKER~MAKERSTAMP~CHECKER~CHECKERSTAMP~MODNO~TXNSTAT~AUTHSTAT~ONCEAUTH</FN>'; 
+msgxml += '      <FN PARENT="BLK_KLEI2" RELATION_TYPE="N" TYPE="BLK_KLEI2_DETAILS">PRODUCT_CODE~RELATIONSHIP</FN>'; 
+msgxml += '    </FLD>'; 
+
+var strScreenName = "CVS_KLEI2";
+var qryReqd = "Y";
+var txnBranchFld = "" ;
+var originSystem = "";
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR DATABINDING *****
+//----------------------------------------------------------------------------------------------------------------------
+ var relationArray = {"BLK_KLEI2" : "","BLK_KLEI2_DETAILS" : "BLK_KLEI2~N"}; 
+
+ var dataSrcLocationArray = new Array("BLK_KLEI2","BLK_KLEI2_DETAILS"); 
+ // Array of all Data Sources used in the screen 
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR QUERY MODE *****
+//----------------------------------------------------------------------------------------------------------------------
+var detailRequired = true ;
+var intCurrentQueryResultIndex = 0;
+var intCurrentQueryRecordCount = 0;
+
+var queryFields = new Array();    //Values should be set inside STDKLEI2.js, in "BlockName__FieldName" format
+var pkFields    = new Array();    //Values should be set inside STDKLEI2.js, in "BlockName__FieldName" format
+queryFields[0] = "BLK_KLEI2__RELATIONSHIP";
+pkFields[0] = "BLK_KLEI2__RELATIONSHIP";
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR AMENDABLE/SUBSYSTEM Fields *****
+//----------------------------------------------------------------------------------------------------------------------
+var modifyAmendArr = new Array(); 
+var closeAmendArr = new Array(); 
+var reopenAmendArr = new Array(); 
+var reverseAmendArr = new Array(); 
+var deleteAmendArr = new Array(); 
+var rolloverAmendArr = new Array(); 
+var confirmAmendArr = new Array(); 
+var liquidateAmendArr = new Array(); 
+var queryAmendArr = new Array(); 
+var authorizeAmendArr = new Array(); 
+//----------------------------------------------------------------------------------------------------------------------
+
+var subsysArr    = new Array(); 
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//***** CODE FOR LOVs *****
+//----------------------------------------------------------------------------------------------------------------------
+var lovInfoFlds = {"BLK_KLEI2__CATEGORY__LOV_KLEARDO_CATEGORY":["BLK_KLEI2__CATEGORY~","","N",""],"BLK_KLEI2_DETAILS__PRODUCT_CODE__LOV_KLEARDO_PRODUCT_CODE":["BLK_KLEI2_DETAILS__PRODUCT_CODE~","","N",""]};
+var offlineLovInfoFlds = {};
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR TABS *****
+//----------------------------------------------------------------------------------------------------------------------
+var strHeaderTabId = 'TAB_HEADER';
+var strFooterTabId = 'TAB_FOOTER';
+var strCurrentTabId = 'TAB_MAIN';
+//--------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR MULTIPLE ENTRY BLOCKS *****
+//----------------------------------------------------------------------------------------------------------------------
+var multipleEntryIDs = new Array("BLK_KLEI2_DETAILS");
+var multipleEntryArray = new Array();
+var multipleEntryCells = new Array();
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR MULTIPLE ENTRY VIEW SINGLE ENTRY BLOCKS *****
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+//***** SCRIPT FOR ATTACHED CALLFORMS *****
+ //----------------------------------------------------------------------------------------------------------------------
+
+ var CallFormArray= new Array(); 
+
+ var CallFormRelat=new Array(); 
+
+ var CallRelatType= new Array(); 
+
+
+ var ArrFuncOrigin=new Array();
+ var ArrPrntFunc=new Array();
+ var ArrPrntOrigin=new Array();
+ var ArrRoutingType=new Array();
+
+
+ // Code for Loading Cluster/Custom js File Starts
+ var ArrClusterModified=new Array();
+ var ArrCustomModified=new Array();
+ // Code for Loading Cluster/Custom js File ends
+
+ArrFuncOrigin["STDKLEI2"]="CUSTOM";
+ArrPrntFunc["STDKLEI2"]="";
+ArrPrntOrigin["STDKLEI2"]="";
+ArrRoutingType["STDKLEI2"]="X";
+
+
+ // Code for Loading Cluster/Custom js File Starts
+ArrClusterModified["STDKLEI2"]="N";
+ArrCustomModified["STDKLEI2"]="Y";
+
+ // Code for Loading Cluster/Custom js File ends
+
+
+ /* Code For OBIEE functionalities */ 
+var obScrArgName  = new Array(); 
+var obScrArgSource  = new Array(); 
+//***** CODE FOR SCREEN ARGS *****
+//----------------------------------------------------------------------------------------------------------------------
+var scrArgName = {};
+var scrArgSource = {};
+var scrArgVals = {};
+var scrArgDest = {};
+//***** CODE FOR SUB-SYSTEM DEPENDENT  FIELDS   *****
+//----------------------------------------------------------------------------------------------------------------------
+var dpndntOnFlds = {};
+var dpndntOnSrvs = {};
+//***** CODE FOR TAB DEPENDENT  FIELDS   *****
+//----------------------------------------------------------------------------------------------------------------------
+//***** CODE FOR CALLFORM TABS *****
+//----------------------------------------------------------------------------------------------------------------------
+var callformTabArray = new Array(); 
+//***** CODE FOR ACTION STAGE DETAILS *****
+//----------------------------------------------------------------------------------------------------------------------
+var actStageArry = {};
+//***** CODE FOR IMAGE FLDSET *****
+//----------------------------------------------------------------------------------------------------------------------
